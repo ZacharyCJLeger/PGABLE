65bd224625bf05f41042370f009042783117fd98
Zachary Charles Joseph Leger <zcjleger@uwaterloo.ca>
2024-09-16 14:16:29 -0400

    Steve's updates to PGAdemo

--- tutorial/PGAdemo.m
+++ tutorial/PGAdemo.m
@@ -19,11 +19,11 @@ disp('a Matlab command.  Type ^C to exit the demo early.');
 disp(' ');
 disp('This brief demonstration is intended to introduce you to the');
 disp('the basic geometric elements of Plane-based Geometric Algebra (planes,');
-disp('lines, and points) as well as lines at infinity and points at infinity');
+disp('lines, and points) as well as lines at infinity and points at infinity.');
 disp('For a more detailed discussion of PGA, see our tutorial.');
 disp('It is assumed that you have run GAdemo before running PGAdemo; if you');
 disp('haven''t done so, please run GAdemo now, and then return to PGAdemo.');
-disp('(The important background that you need from GAdemo are outer, inner,');
+disp('(The important background that you need from GAdemo are the outer, inner,');
 disp('and geometric products.)');
 
 disp(' ');GAprompt;disp(' ');
@@ -33,8 +33,8 @@ disp('graphical representations of the geometrical objects.  PGA differs from');
 disp('OGA in two important ways.  First, while the basis for PGA contains ');
 disp('the basis elements of OGA, e1,e2,e3, in addition, in PGA there is');
 disp('a fourth basis vector, e0, that is a null vector, meaning that ');
-disp('e0.e0=0 even though e0 is not the zero vector.  You can test this now');
-disp('in PGA by typing   inner(e0,e0)  and   e0==0');
+disp('e0.e0=0 even though e0 is not the zero vector.  You can test these');
+disp('properties of e0 in PGA now by typing   inner(e0,e0)  and   e0==0');
 
 disp(' ');GAprompt;disp(' ');
 
@@ -55,7 +55,6 @@ disp(' ');GAprompt;disp(' ');
 
 disp('Let''s first draw the three coordinate planes, each offset from');
 disp('the origin by different amounts:');
-pause(2);
 disp(' >> clf;');
 clf;
 disp(' >> n1=e1 - e0; draw(n1,''r'');');
@@ -71,7 +70,7 @@ n3 = e3-0.25*e0;
 draw(n3,'b');
 drawnow;
 disp(' ');
-pause(1);
+pause(2);
 disp('In the graphics window, you should see three planes.  The red one');
 disp('represents the plane n1 with normal e1 offset from the origin by a distance');
 disp('of 1; the magenta represents the plane n2 with normal e2 offset from the');
@@ -101,7 +100,7 @@ disp('(the magenta plane).');
 pause(2);
 disp('The second thing to note is that in the text window, the line L12 is');
 disp('a bivector that has a Euclidean part, e1^e2, and an e0 part, e0(0.5e1-e2).');
-disp('The Euclidean part represents is the bivector perpendicular to the ');
+disp('The Euclidean part represents the bivector perpendicular to the ');
 disp('direction of the line.');
 disp('Computing the Euclidean dual (by dividing by I3) of this bivector,');
 disp('(e1^e2)/I3, gives the direction vector of the line.  Go ahead and ');
@@ -133,16 +132,15 @@ disp('intersection of n1, n2, and n3.');
 pause(3);disp(' ');
 
 disp('Second, in the text window, we see that P is a trivevector with a ');
-disp('Euclidean part e1^e2^e3, and an e0 part.  The Euclidean bivector');
+disp('Euclidean part e1^e2^e3, and an e0 part.  The Euclidean trivector');
 disp('e1^e2^e3 represents the point at the origin.  In a normalized point,');
 disp('the coefficient of e1^e2^e3 is 1, as in this example.');
 pause(2); disp(' ');
 disp('The e0 part of the point contains the location of the point.  Basically,');
 disp('each e0 term has a missing basis vector; the coefficient of the term is');
 disp('the coordinate of the missing basis vector.  For example, P, the point we');
-disp('have created, has e0(-0.25e1^e2 + 0.5*e1^e3 + -e2^e3), and thus has');
-disp('coordinates 0.25 z, 0.5 y, and 1 x (the minus signs appear in the');
-disp('equation because of the basis we have chosen to display things).');
+disp('have created, has e0(0.25e1^e2 + 0.5*e1^e3 + e2^e3), and thus has');
+disp('coordinates 0.25 e3, 0.5 e2, and 1 e1');
 
 disp(' ');GAprompt;disp(' ');
 
@@ -170,25 +168,40 @@ disp('draw(nv);');
 disp(' ');
 disp('In the text window, note that the line nv has only an e0 component,');
 disp('where as the line we saw earlier also had a Euclidean component.');
-disp('The e0 componenet of the earlier line was an offset from the origin');
+disp('The e0 component of the earlier line was an offset from the origin');
 disp('to the closest point on the line.  For nv, the e0 component gives');
 disp('the direction to the line at infinity.');
 disp(' ');
-disp('In the graphics window, it is unclear how to draw a line at infinity.');
-disp('We have chosen to draw nv as a dashed line around the Matlab drawing ');
-disp('volume.  Technically, where this line would appear depends on where');
-disp('you view it from; we have chosen to draw this line as if viewed from');
-disp('the center of the Matlab drawing volume.');
+disp('In the graphics window, we have chosen to draw nv as a dashed line ');
+disp('around the Matlab drawing volume.  Technically, where this line would');
+disp('appear depends on where you view it from; we have chosen to draw this');
+disp('line as if viewed from the center of the Matlab drawing volume.');
 
 disp(' ');GAprompt;disp(' ');
 
 clc
-disp('We intersect a line at infinity, with another plane, giving us a');
-disp('');
-disp('we refer to as a vanishing line.  Let''s clear the screen and draw two');
-disp('parallel planes:'); disp(' ');
+disp('If we intersect a line at infinity with another plane, we get a');
+disp('point at infinity, also known as a vanishing point:');
+disp(' ');
+disp('pv = nv^e2');
+pv = nv^e2
+draw(pv);
+view([-15 40])
 
 disp(' ');GAprompt;disp(' ');
-disp('This is the end of our brief introduction to PGA in PGABLE.');
-disp('For a further discussion on PGA, see our tutorial.');
+
+disp('Comparing pv to the P we saw earlier,');
+P
+disp('we see that pv only has an e0 component and doesn''t have a purely Euclidean');
+disp('component.  The e0 component of pv is a bivector that is perpendicular to');
+disp('the direction in which pv lies at infinity.');
+disp('We draw pv as a star on the boundary of the Matlab box, with a short ');
+disp('line segment to indicate this direction.  As expected, pv lies on the ');
+disp('line nv (since pv is the intersection of nv and a plane).');
+
+disp(' ');GAprompt;disp(' ');
+disp('This is the end of our brief introduction to PGA in PGABLE, showing.');
+disp('planes, lines, points, vanishing lines, and vanishing planes.  ');
+disp('Transformations in PGA are similar to OGA, but for a further discussion');
+disp('on these transformations and on PGA in general, see our tutorial.');
 catch ; end
