46ecc0c1d4b94a48aa86a24b3a0c8c6635506be9
Zachary Charles Joseph Leger <zcjleger@uwaterloo.ca>
2024-09-24 12:17:07 -0400

    Fix increasing order in OGA

--- OGA.m
+++ OGA.m
@@ -566,7 +566,7 @@ classdef OGA < GA
             [s, pl] = GA.charify_val_(p.m(4), 'e3', s, pl);
                         
             if GA.compact_notation()
-                if ~OGA.increasing_order()
+                if OGA.increasing_order()
                     [s, pl] = GA.charify_val_(p.m(7), 'e23', s, pl);
                     [s, pl] = GA.charify_val_(-p.m(6), 'e31', s, pl);
                     [s, pl] = GA.charify_val_(p.m(5), 'e12', s, pl);
@@ -582,7 +582,7 @@ classdef OGA < GA
                     [s, pl] = GA.charify_val_(p.m(8), 'e123', s, pl);
                 end
             else
-                if ~OGA.increasing_order()
+                if OGA.increasing_order()
                     [s, pl] = GA.charify_val_(p.m(5), 'e1^e2', s, pl);
                     [s, pl] = GA.charify_val_(p.m(6), 'e1^e3', s, pl);
                     [s, pl] = GA.charify_val_(p.m(7), 'e2^e3', s, pl);
