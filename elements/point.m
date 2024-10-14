c7469878fa510b9bcb9f0b090045e0623d7d63b7
Zachary Charles Joseph Leger <zcjleger@uwaterloo.ca>
2024-10-14 14:07:29 -0400

    Specify point using OGA vector

--- elements/point.m
+++ elements/point.m
@@ -1,5 +1,10 @@
 function e = point(x, y, z, model)
-    % point
+    %POINT - Create a point.
+    %   Either supply a (x, y, z) coordinates and the model to create a point in said
+    %   model (if points are a valid geometric element in the model).
+    %   Alternatively, supply an OGA vector as the first argument and the model as the
+    %   second argument to create a point in the specified model whose coordinates correspond
+    %   to the OGA vector.
 
     % PGABLE, Copyright (c) 2024, University of Waterloo
     % Copying, use and development for non-commercial purposes permitted.
@@ -8,11 +13,37 @@ function e = point(x, y, z, model)
     %
     %          This software is unsupported.
     arguments
-        x (1, 1) {mustBeNumeric};
-        y (1, 1) {mustBeNumeric};
-        z (1, 1) {mustBeNumeric};
+        x;
+        y = [];
+        z = [];
         model = GA.model;
     end
+
+    if nargin >= 3
+        if isnumeric(x) && isnumeric(y) && isnumeric(z)
+            
+        else
+            error("If supplying (x, y, z) to point, the values must be numeric")
+        end
+    elseif nargin == 1
+        if isa(x, "OGA")
+            z = x.getz();
+            y = x.gety();
+            x = x.getx();
+        end
+    elseif nargin == 2
+        if isa(y, "GA") || isa(y, "string")
+            model = y;
+        end
+        if isa(x, "OGA")
+            z = x.getz();
+            y = x.gety();
+            x = x.getx();
+        end
+    else
+       error("Incorrect number of arguments provided") 
+    end
+
     if isa(model, "GA")
         model = modelname(model);
     end
