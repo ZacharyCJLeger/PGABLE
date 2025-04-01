7e8263a2cb6fdd0714c411a54b611e0b1ebcb4ff
Zachary Charles Joseph Leger <zcjleger@uwaterloo.ca>
2025-04-01 10:22:07 -0400

    Contents update

--- Contents.m
+++ Contents.m
@@ -6,9 +6,10 @@
 %   The tutorial was largely written by Stephen Mann (smann@uwaterloo.ca).
 %
 %   GA is a parent class of all geometric algebra models.
-%   The models OGA and PGA are currently implemented as child classes of GA.
+%   The models OGA, PGA, and CGL are currently implemented as child classes of GA.
 %   Type help(OGA) and help(PGA) for information about each model.
-%   To switch between OGA and PGA, run GA.model(OGA) or GA.model(PGA)
+%   To switch between OGA and PGA, run GA.model(OGA), GA.model(PGA), or
+%   GA.model(CGA).
 %   You may want to run GAdemo for a brief introduction to OGA, and
 %   PGAdemo for a brief introduction to PGA.
 %
